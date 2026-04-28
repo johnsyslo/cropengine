945c774b2c4200f9ef8de0ee7f140af7f93f8287
John <john@johnsyslo.com>
2026-04-27 21:25:28 -0500

    feat(modmenu): maxDelay

--- src/main/java/com/john/cropengine/config/ModConfig.java
+++ src/main/java/com/john/cropengine/config/ModConfig.java
@@ -10,8 +10,12 @@ public class ModConfig implements ConfigData {
     public boolean enabled = true;
 
     @ConfigEntry.Gui.Tooltip
-    @ConfigEntry.BoundedDiscrete(min = 1, max = 50)
-    public int minDelay = 20;
+    @ConfigEntry.BoundedDiscrete(min = 1, max = 25)
+    public int minDelay = 10;
+
+    @ConfigEntry.Gui.Tooltip
+    @ConfigEntry.BoundedDiscrete(min = 25, max = 50)
+    public int maxDelay = 30;
 
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
